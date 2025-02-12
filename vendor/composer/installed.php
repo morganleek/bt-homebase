45ef903ece95c8d3e085b72a75f9a648cf9fded7
Morgan Leek <morganleek@gmail.com>
2025-02-12 20:57:07 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4b68b0dc45879be25d662f7c83cba62226a76d3d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b68b0dc45879be25d662f7c83cba62226a76d3d',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.5.0',
+      'version' => '3.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '944dd17df10e2e534e62ca3f5f20e550c2058a63',
+    ),
+  ),
+);
