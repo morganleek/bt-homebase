416acaa2841d7a68a346daef8bbc86f7bb0544a7
Morgan Leek <morganleek@gmail.com>
2025-03-26 14:42:44 +0800

    home rough

--- README.md
+++ README.md
@@ -44,3 +44,33 @@ Set Git FTP settings with 'syncroot' pointing at packages directory. You may nee
         insecure = 1
         syncroot = package/{directory_name}
 ```
+
+### Fonts
+Gotham Narrow
+
+16/22 Med Nav
+
+56/64 Bol Heading 1             3.5rem          1.142857em
+36/40
+34/42 Bol Heading 2             2.125rem        1.235294em
+28/36
+24/30 Med Heading 3             1.5rem          1.25em
+16/22
+20/24 Med Heading 4             1.25rem         1.2em
+16/22
+16/24 Med Heading 5             1rem            1.5em
+16/22
+14/22 Med Heading 6             0.875rem        1.571428em
+14/22
+
+20/30 Med Paragraph Large       1.25rem         1.5em
+16/24
+16/24 Med Paragraph             1rem            1.5em
+16/24
+
+14/22 Med Excerpt               0.875rem        1.571428em
+13/16 Med Date                  0.8125rem       1.230769em
+20/16 Med Author                1.25rem         0.8em
+
+16/24 Med Button                1rem            1.5em
+
--- functions.php
+++ functions.php
@@ -48,6 +48,11 @@ class BoneThemeInit
 
 		// Gallery Sliders
 		// add_action( 'wp_print_styles', [ $this, 'update_styles' ] );
+
+		// Copyright Year 
+		if ( ! is_admin() ) {
+			add_action( 'render_block', [$this, 'bones_theme_render_block'], 5, 2 );
+		}
 	}
 
 	public function plugin_enqueue()
@@ -58,6 +63,34 @@ class BoneThemeInit
 
 		// Inline styles for fonts
 		// wp_add_inline_style( 'bones-theme-style', $this->bones_theme_get_font_face_styles() );
+	}
+
+	public function bones_theme_render_block( $block_content, $block ) {
+		// Copyright Year
+		if( $block['blockName'] === "core/paragraph" ) {
+			$year_regex = "/({YEAR})/i";
+			$block_content = preg_replace( $year_regex, date('Y'), $block_content );
+		}
+
+		// Change Hamburger
+		if( $block['blockName'] === "core/navigation" ) {
+			$svg_regex = "/<svg.*?\/svg>/i";
+			$svg = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 12H21M3 6H21M9 18H21" stroke="#38332F" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"/></svg>';
+			$block_content = preg_replace( $svg_regex, $svg, $block_content );
+		}
+
+		// Mini-cart Icon
+		if( 
+			$block['blockName'] === "woocommerce/mini-cart"
+		) {
+			$svg_regex = "/<svg.*?class=\"wc-block-mini-cart__icon\".*?>.*?\/svg>/i";
+			$svg = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="wc-block-mini-cart__icon"><path d="M2 2H3.30616C3.55218 2 3.67519 2 3.77418 2.04524C3.86142 2.08511 3.93535 2.14922 3.98715 2.22995C4.04593 2.32154 4.06333 2.44332 4.09812 2.68686L4.57143 6M4.57143 6L5.62332 13.7314C5.75681 14.7125 5.82355 15.2031 6.0581 15.5723C6.26478 15.8977 6.56108 16.1564 6.91135 16.3174C7.30886 16.5 7.80394 16.5 8.79411 16.5H17.352C18.2945 16.5 18.7658 16.5 19.151 16.3304C19.4905 16.1809 19.7818 15.9398 19.9923 15.6342C20.2309 15.2876 20.3191 14.8247 20.4955 13.8988L21.8191 6.94969C21.8812 6.62381 21.9122 6.46087 21.8672 6.3335C21.8278 6.22177 21.7499 6.12768 21.6475 6.06802C21.5308 6 21.365 6 21.0332 6H4.57143ZM10 21C10 21.5523 9.55228 22 9 22C8.44772 22 8 21.5523 8 21C8 20.4477 8.44772 20 9 20C9.55228 20 10 20.4477 10 21ZM18 21C18 21.5523 17.5523 22 17 22C16.4477 22 16 21.5523 16 21C16 20.4477 16.4477 20 17 20C17.5523 20 18 20.4477 18 21Z" stroke="#38332F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+			$block_content = preg_replace( $svg_regex, $svg, $block_content );
+		}
+
+		// error_log( $block['blockName'], 0 );
+
+		return $block_content;
 	}
 
 	public function bones_theme_support()
@@ -130,11 +163,11 @@ class BoneThemeInit
 			'label' => __( 'Overlap', 'bones_name' )
 		] );
 
-		// Cover
-		// register_block_style( 'core/cover', [
-		//   'name' => 'banner-reversed',
-		//   'label' => __( 'Reversed', 'bones_name' ),
-		// ] );
+		// Slider
+		register_block_style( 'getwid/content-slider-slide', [
+		  'name' => 'overflow',
+		  'label' => __( 'Overflow', 'bones_name' ),
+		] );
 
 		// Columns
 		// register_block_style( 'core/columns', [
