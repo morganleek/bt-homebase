11a6b31fae58f5b974f2aea494954506ba10f883
Morgan Leek <morganleek@gmail.com>
2025-06-03 14:15:31 +0800

    remove heading from courses

--- package/bt-homebase/template-parts/blocks/course-information.php
+++ package/bt-homebase/template-parts/blocks/course-information.php
@@ -5,13 +5,8 @@
 		$terms_filtered = array_filter( $terms, fn( $t ) => $t->slug === "courses" );
 		if( count( $terms_filtered) > 0 ) {
 			?>
-				<div class="wp-block-course-information wp-block-group alignwide has-white-background-color has-background has-global-padding is-layout-constrained wp-block-group-is-layout-constrained" style="margin-top:0;margin-bottom:0">
-					<div class="wp-block-group alignwide has-brand-dust-background-color has-background is-layout-flow wp-block-group-is-layout-flow" style="border-top-left-radius:24px;border-top-right-radius:24px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-						<h3 class="wp-block-heading">Course information</h3>
-					</div>
-				</div>
 				<?php if( $course_structure = get_field('column_one', get_the_ID()) ) { ?>
-					<div class="wp-block-course-information-structure wp-block-group alignwide has-white-background-color has-background is-layout-flow wp-block-group-is-layout-flow" style="border-radius:16px;margin-top:0;margin-bottom:var(--wp--preset--spacing--30);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+					<div class="wp-block-course-information-structure wp-block-group alignwide has-white-background-color has-background is-layout-flow wp-block-group-is-layout-flow" style="border-radius:0 0 16px 16px;margin-top:0;margin-bottom:var(--wp--preset--spacing--30);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
 						<h3 class="wp-block-heading">Structure</h3>
 						<div><?php print $course_structure; ?></div>
 					</div>
